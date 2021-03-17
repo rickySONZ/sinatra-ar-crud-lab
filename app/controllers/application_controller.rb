877e580113e77ee0a054e95f964990783ffc5f4d
Ryan Erricson <ryanerricson@gmail.com>
2021-03-17 13:44:03 -0400

    commit tree af61bacdf5375a1c31d37b4e5c4963e956b92e5d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,59 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:title], content: params[:content])
+
+    if article.save
+      redirect "articles/#{article.id}"
+    else
+      redirect '/articles/new'
+    end
+  end
+  
+
+
+  get '/articles/:id' do
+    @articles = Article.all
+    
+    @article = @articles.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    article.title = params["title"]
+    article.content = params["content"]
+
+    if article.save
+      redirect "articles/#{article.id}"
+    else
+      redirect "/articles/#{article.id}"
+    end
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    article.destroy
+
+    redirect '/articles'
+  end
+
 end
